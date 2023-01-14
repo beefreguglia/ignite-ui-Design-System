edd6c31195773cbb7be59b4cff8ce2ef137eb3d6
bernardo <beefreguglia@gmail.com~>
2023-01-14 16:21:57 -0300

    Create avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from 'react'
+import { User } from 'phosphor-react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+
+export interface AvatarImageProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(avatarImageProps: AvatarImageProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...avatarImageProps} />
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
