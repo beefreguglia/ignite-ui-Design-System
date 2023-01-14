aa991b9ab250ab958b1d453ab1670c8124f31cdb
bernardo <beefreguglia@gmail.com~>
2023-01-14 18:25:48 -0300

    Create text input component

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Card, Text, TextInput, TextInputProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Text Input',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Card
+          as="label"
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
+          <Text size="sm">Email address</Text>
+          {Story()}
+        </Card>
+      )
+    },
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'cal.com/',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
