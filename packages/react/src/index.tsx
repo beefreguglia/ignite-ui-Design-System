4d9f97014f6a1c778daf40ec9b9c7d35b2c16f10
bernardo <beefreguglia@gmail.com~>
2023-01-14 15:57:21 -0300

    Remove button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,30 +1,3 @@
-import { ComponentProps } from 'react'
-import { styled } from './styles'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
-  borderRadius: '$md',
-  border: 0,
-  fontWeight: '$bold',
-  color: '$white',
-
-  variants: {
-    size: {
-      small: {
-        padding: '$2 $4',
-        fontSize: 14,
-      },
-      big: {
-        padding: '$3 $6',
-        fontSize: 16,
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: 'small',
-  },
-})
-
-export type ButtonProps = ComponentProps<typeof Button>
+export * from './components/Card'
+export * from './components/Text'
+export * from './components/Heading'
