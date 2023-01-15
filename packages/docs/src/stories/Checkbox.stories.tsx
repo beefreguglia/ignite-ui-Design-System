08e797f346894a958dbe32254a9fba672bb09b39
bernardo <beefreguglia@gmail.com~>
2023-01-15 13:19:49 -0300

    Create checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,23 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Card, Text, Checkbox, CheckboxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Card
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use</Text>
+        </Card>
+      )
+    },
+  ],
+} as Meta<CheckboxProps>
+
+export const Primary: StoryObj<CheckboxProps> = {}
